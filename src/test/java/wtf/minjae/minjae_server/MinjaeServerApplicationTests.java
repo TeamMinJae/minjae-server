e7861fa80dd3064f96fdcbde2fec0d15a3bbee67
haiseong <wjdgotjd9908@gmail.com>
2025-07-25 16:38:30 +0900

    chore: 스프링 프로젝트 세팅

---
+++ src/test/java/wtf/minjae/minjae_server/MinjaeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package wtf.minjae.minjae_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinjaeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
