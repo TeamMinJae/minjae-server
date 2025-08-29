beb8844798ba82d630e4c1f812cf91ec736f738f
tackyu <tack970913@gmail.com>
2025-08-29 16:12:49 +0900

    refactor: include path filed

--- src/main/java/wtf/minjae/minjae_server/domain/Video.java
+++ src/main/java/wtf/minjae/minjae_server/domain/Video.java
@@ -1,26 +1,13 @@
 package wtf.minjae.minjae_server.domain;
 
-import java.io.File;
 import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Getter
 public class Video {
 
-    private static final String VIDEO_PATH_FORMAT = "static/videos/%s.mp4";
+    private final String path;
 
-    private final File videoFile;
-
-    public Video(String baseVideo) {
-        log.info("basevideo에 맞는 원본 비디오 파일 찾아오기");
-        this.videoFile = new File(String.format(VIDEO_PATH_FORMAT, baseVideo));
-        validateVideo(videoFile);
-    }
-
-    private void validateVideo(File videoResource) {
-        if (!videoResource.exists()) {
-            throw new IllegalArgumentException("Video does not exist");
-        }
+    public Video(String path) {
+        this.path = path;
     }
 }
